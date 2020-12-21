12aefb0afbe65367c9c92af81412c79b3e468c98
Hagen Strahl <hagen.strahl@gmail.com>
2020-12-21 17:20:42 +0100

    add contact formular placeholder

--- schemas/documents/article.js
+++ schemas/documents/article.js
@@ -8,6 +8,7 @@ import link from "../fields/link";
 import externalLink from "../fields/externalLink";
 import { withCTA } from "../fields/fieldExtender";
 import internalLink from "../fields/internalLink";
+import contactFomular from "../fields/contactFomular";
 
 const supportedLanguages = [
   { id: "de", title: "Deutsch", isDefault: true },
@@ -51,6 +52,7 @@ const fields = [
         fields: [...image.fields, link(["article"])],
       },
       imageGallery,
+      contactFomular,
     ],
   },
 ];
